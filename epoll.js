091329b8f45e7b9835c04a9db811098b3ef4db46
YuSHEN <magicyu90@gmail.com>
2015-08-24 21:57:32 +0200

    add greeting function

--- epoll.js
+++ epoll.js
@@ -1,23 +1,24 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    //create a easy template
+    Template.hello.greeting = function () {
+
+        return "Welcome to epoll.";
+    };
+
+    //events
+    Template.hello.events({
+        "click input": function () {
+            if (typeof console !== "undefined")
+                console.log("You pressed the button");
+        }
+    });
+
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
 }
