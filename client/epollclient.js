be6e616c08c0571a368284a14018856008bbb506
YuSHEN <magicyu90@gmail.com>
2015-08-25 00:55:25 +0200

    add question event

--- client/epollclient.js
+++ client/epollclient.js
@@ -2,14 +2,29 @@
  * Created by shen on 15/8/24.
  */
 
-Template.hello.greeting = function () {
-    return "The Missing Meteor Tutorial!!!";
-};
+/*
+ Template.hello.greeting = function () {
+ return "The Missing Meteor Tutorial!!!";
+ };
 
-Template.hello.events({
-    'click input' : function () {
-        // template data, if any, is available in 'this'
-        if (typeof console !== 'undefined')
-            console.log("You pressed the button");
+ Template.hello.events({
+ 'click input' : function () {
+ // template data, if any, is available in 'this'
+ if (typeof console !== 'undefined')
+ console.log("You pressed the button");
+ }
+ });*/
+
+Template.addQuestion.events({
+    "click input.add-question": function (event) {
+
+        event.preventDefault();
+        var questionText = document.getElementById('questionText').value;
+
+        Meteor.call("addQuestion", questionText, function (error, questionId) {
+
+            console.log("add question with id..." + questionId);
+        });
+        document.getElementById('questionText').value = "";
     }
 });
--- server/epollserver.js
+++ server/epollserver.js
@@ -1,6 +1,23 @@
 /**
  * Created by shen on 15/8/24.
  */
+
+Questions = new Mongo.Collection("questions");
+
 Meteor.startup(function () {
     // code to run on server at startup
+});
+
+Meteor.methods({
+
+    addQuestion: function (text) {
+        console.log("add question");
+        var questionId = Questions.insert({
+            'questionText': text,
+            'submittedOn': new Date(),
+            //'submittedBy': Meteor.user().profile.name + "_" + Meteor.userId()
+            'submittedBy':Meteor.userId()
+        });
+        return questionId;
+    }
 });
