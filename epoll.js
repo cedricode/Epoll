29244ed5c121a50d1a8d5f353a5706750c2737ff
YuSHEN <magicyu90@gmail.com>
2015-08-24 22:41:27 +0200

    show twitter login button

--- epoll.js
+++ epoll.js
@@ -1,19 +1,19 @@
 if (Meteor.isClient) {
 
 
-    //create a easy template
-    Template.hello.greeting = function () {
+
+/*    Template.hello.greeting = function () {
 
         return "Welcome to epoll.";
     };
 
-    //events
+
     Template.hello.events({
         'click input': function () {
             if (typeof console !== "undefined")
                 console.log("You pressed the button");
         }
-    });
+    });*/
 
 }
 
